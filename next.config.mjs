27a8d10d38ef3a4ad2cf111d6fccba1b3b4a3ab9
giorgiabes <abesadze.george@gmail.com>
2024-06-27 14:57:34 +0400

    ch10a

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
- 
+
 const nextConfig = {
   experimental: {
-    ppr: 'incremental',
-  },
+    ppr: true,
+  }
 };
- 
-module.exports = nextConfig;
+
+export default nextConfig;
